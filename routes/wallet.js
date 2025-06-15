e7ed5ef26729d57894fe28ff11554abc75872762
juhanncheong <hjim1188@gmail.com>
2025-06-15 13:47:26 -0400

    final fix

--- routes/wallet.js
+++ routes/wallet.js
@@ -9,7 +9,7 @@ router.get("/balance/:userId", async (req, res) => {
     const user = await User.findById(req.params.userId);
     if (!user) return res.status(404).json({ message: "User not found" });
 
-    res.json({ btcBalance: user.btcBalance });
+    res.json({ btcBalance: user.balance });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
