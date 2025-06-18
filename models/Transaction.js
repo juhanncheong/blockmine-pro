93961e185fc19839182a3f34c1f030c9e5e590b7
juhanncheong <hjim1188@gmail.com>
2025-06-17 21:54:38 -0400

    k

--- models/Transaction.js
+++ models/Transaction.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const TransactionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  type: { type: String, enum: ["deposit", "withdraw", "purchase", "referral-commission", "mining"], required: true },
+  type: { type: String, enum: ["deposit", "withdraw", "purchase", "referral-commission", "mining", "earnings"], required: true },
   amount: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now }
 });
