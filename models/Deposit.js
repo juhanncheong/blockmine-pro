eebbe2a63b3e8909664e1311b05a4b700c48d948
Robert Cheong <hjim1188@gmail.com>
2025-11-14 06:33:42 -0500

    kkk

--- models/Deposit.js
+++ models/Deposit.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const depositSchema = new mongoose.Schema({
   userId:              { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   coin:                { type: String, required: true },
@@ -18,3 +20,5 @@ const depositSchema = new mongoose.Schema({
 
   createdAt:           { type: Date, default: Date.now }
 });
+
+module.exports = mongoose.model("Deposit", depositSchema);
